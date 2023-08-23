f439b17e2785477755d7398e523363ec17b4520b
kashif tufail <kashif.tufail17@gmail.com>
2023-08-23 22:35:17 +0500

    welcome email using sidekiq

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         
+  after_create :send_welcome_email       
+
+  def send_welcome_email
+    WelcomeEmailJob.perform_in(30.seconds, self.id)
+  end  
 end
