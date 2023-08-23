5c4bc4d1f95c209a18acf289a8b9710366758d4c
kashif tufail <kashif.tufail17@gmail.com>
2023-08-23 22:17:39 +0500

    configure sign up, sign in, forgot password using devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,17 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+   # SMTP settings for gmail
+   config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'kashif.tufail17@gmail.com',
+    :password             => '',
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
